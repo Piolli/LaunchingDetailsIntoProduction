947e7446703ed06747f4239f30810eca73d9a957
Alexandr <pocharua@mail.ru>
2020-03-22 14:27:11 +0700

    Initial Commit

---
+++ LaunchingDetailsIntoProduction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LaunchingDetailsIntoProduction
+//
+//  Created by Александр Камышев on 22.03.2020.
+//  Copyright © 2020 Александр Камышев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
